163e71f001067045c159403ae7ad3e8a8c707f09
Julia Tufts <juliatufts@gmail.com>
2014-11-11 13:18:41 -0500

    added first draft of poisson-disc points

--- points.js
+++ points.js
@@ -3,7 +3,12 @@
 	var ctx = canvas.getContext("2d");
 	var screenSize = { width : canvas.width, height : canvas.height};
 
-	// returns a list of n random points
+	var Point = function(x, y) {
+		this.x = x;
+		this.y = y;
+	};
+
+	// returns a list of n uniformly random points
 	function randomPoints(n) {
 		var points = [];
 		for (var i = 0; i < n; i++){
@@ -75,8 +80,112 @@
 		return points;
 	}
 
+	// Poisson-disc sampling
+	function poissonDisc(r, k) {
+
+		// grid variables
+		var cellSize = (r / Math.sqrt(2));
+		var gridWidth = Math.ceil(screenSize.width / cellSize);
+		var gridHeight = Math.ceil(screenSize.height / cellSize);
+		var grid = [];
+
+		// function for finding the grid cell of a given point
+		function findCell(point) {
+			var tempX = Math.floor(point.x / cellSize);
+			var tempY = Math.floor(point.y / cellSize);
+			return [tempX, tempY];
+		}
+		
+		// initialize the grid
+		for (var i = 0; i < gridHeight; i++) {
+			var row = [];
+			for (var j = 0; j < gridWidth; j++) {
+				row.push(null);
+			}
+			grid.push(row);
+		}
+
+		// start with one random point
+		var activePoints = randomPoints(1);
+		var finalPoints = [];
+
+		// and add it to the grid
+		var cellCoords = findCell(activePoints[0]);
+		grid[cellCoords[1]][cellCoords[0]] = activePoints[0];
+
+		// while we still have active points
+		while (activePoints.length > 0) {
+
+			// pick a random active point
+			var index = Math.floor(Math.random() * activePoints.length)
+			var current = activePoints[index];
+			var allPointsTooClose = true;
+
+			// generate k sample points
+			for (var i = 0; i < k; i++) {
+
+				var angle = Math.random() * 2 * Math.PI;
+				var radius = Math.random() * r + r;
+
+				// new sample point
+				var x = Math.floor(current.x + Math.cos(angle) * radius);
+				var y = Math.floor(current.y + Math.sin(angle) * radius);
+				// if it's offscreen, go on to the next sample point
+				if (x < 0 || x >= screenSize.width || y < 0 || y >= screenSize.height) {
+					continue;
+				}
+				var p = new Point(x, y);
+
+				// find its cell position in the grid
+				var cell = findCell(p);
+
+				// check surrounding cells
+				var directions = [[0, 1], [0, 2], [0, -1], [0, -2],
+								  [1, 0], [2, 0], [-1, 0], [-2, 0],
+								  [1, 1], [1, -1], [-1, 1], [-1, -1],
+								  [2, 1], [2, -1], [-2, 1], [-2, -1],
+								  [1, 2], [1, -2], [-1, 2], [-1, -2]];
+
+				var foundClosePoint = false;
+				for (var j = 0; j < directions.length; j++) {
+
+					var sampleCell = [cell[0] + directions[j][0],
+									  cell[1] + directions[j][1]];
+
+					// check if the cell is within range
+					if (sampleCell[0] < 0 || sampleCell[0] >= gridWidth ||
+						sampleCell[1] < 0 || sampleCell[1] >= gridHeight) {
+						continue;
+					} else if (grid[sampleCell[1]][sampleCell[0]]) {
+						var distance = dist(p, grid[sampleCell[1]][sampleCell[0]]);
+						if (distance < r) {
+							foundClosePoint = true;
+							break;
+						}
+					}
+				}
+
+				if (!foundClosePoint) {
+					activePoints.push(p);
+					grid[cell[1]][cell[0]] = p;
+					allPointsTooClose = false;
+					break;
+				}
+			}
+
+			// after we've checked all the samples
+			if (allPointsTooClose) {
+				finalPoints.push(current);
+				activePoints.splice(index, 1);
+			}
+		}
+		return finalPoints;
+	}
+
 	// plot the points resulting from best candidate
-	drawPoints(bestCandidate(1000, 50));
+	// drawPoints(bestCandidate(1000, 50));
+
+	drawPoints(poissonDisc(300, 10));
 
 })();
 
