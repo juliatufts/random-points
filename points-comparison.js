222abd522a79cd2c329819b71415f6e17062ad29
Julia Tufts <juliatufts@gmail.com>
2014-11-13 14:05:42 -0500

    fixed poisson-disc method, added comparison files that display both best candidate and poisson-disc mthods at once

--- points-comparison.js
+++ points-comparison.js
@@ -1,15 +1,22 @@
 ;(function() {
-	var canvas = document.getElementById("canvasId");
-	var ctx = canvas.getContext("2d");
-	var screenSize = { width : canvas.width, height : canvas.height};
+	// top canvas
+	var canvasTop = document.getElementById("bestCand");
+	var ctxTop = canvasTop.getContext("2d");
+	var screenSizeTop = { width : canvasTop.width, height : canvasTop.height};
 
+	// bottom canvas
+	var canvasBottom = document.getElementById("poisson");
+	var ctxBottom = canvasBottom.getContext("2d");
+	var screenSizeBottom = { width : canvasBottom.width, height : canvasBottom.height};
+
+	// Point constructor
 	var Point = function(x, y) {
 		this.x = x;
 		this.y = y;
 	};
 
 	// returns a list of n uniformly random points
-	function randomPoints(n) {
+	function randomPoints(n, screenSize) {
 		var points = [];
 		for (var i = 0; i < n; i++){
 			var x = Math.floor(Math.random() * screenSize.width);
@@ -21,7 +28,7 @@
 	}
 
 	// draws the list of random points given as red circles on the canvas
-	function drawPoints(points){
+	function drawPoints(points, ctx){
 		ctx.fillStyle = "#FF0000";
 		var size = 3;
 
@@ -42,15 +49,23 @@
 		return Math.sqrt(dx * dx + dy * dy);
 	}
 
+	// function for finding the grid cell of a given point
+	function findCell(point, cellSize) {
+		var tempX = Math.floor(point.x / cellSize);
+		var tempY = Math.floor(point.y / cellSize);
+		return [tempX, tempY];
+	}
+
+
 	//------------------------- Mitchell’s best-candidate algorithm for sampling points
 	//---------------------------------------------------------------------------------
-	function bestCandidate(n, sampleSize){
+	function bestCandidate(n, sampleSize, screenSize){
 		// the list of plot points
-		points = randomPoints(1);
+		points = randomPoints(1, screenSize);
 
 		// choose n-1 more points
 		for (var i = 1; i < n; i++){
-			var sample = randomPoints(sampleSize);
+			var sample = randomPoints(sampleSize, screenSize);
 
 			// for each sample point
 			var minDistances = [];
@@ -81,18 +96,9 @@
 		return points;
 	}
 
-
-
-	// function for finding the grid cell of a given point
-	function findCell(point, cellSize) {
-		var tempX = Math.floor(point.x / cellSize);
-		var tempY = Math.floor(point.y / cellSize);
-		return [tempX, tempY];
-	}
-
 	//---------------------------------------------- Poisson-disc sampling
 	//--------------------------------------------------------------------
-	function poissonDisc(r, k) {
+	function poissonDisc(r, k, screenSize) {
 
 		// grid variables
 		var cellSize = (r / Math.sqrt(2));
@@ -193,8 +199,11 @@
 		return finalPoints;
 	}
 
+	// plot the points resulting from best candidate
+	drawPoints(bestCandidate(500, 30, screenSizeTop), ctxTop);
+
 	// plot the points resulting from poisson disc method
-	drawPoints(poissonDisc(10, 10));
+	drawPoints(poissonDisc(15, 30, screenSizeBottom), ctxBottom);
 
 })();
 
